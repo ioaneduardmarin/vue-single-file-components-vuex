c8359f2856311639ad19f1e59b7c65e1c0de3c00
Adrian C <adrian.cearnau@xconta.ro>
2021-05-10 13:59:20 +0300

    Bring back localization button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,21 @@
 import '../node_modules/semantic-ui-css/semantic.css';
 import i18n from '../src/i18n/i18nInstance';
 
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', right: 'en', title: 'English' },
+        { value: 'ro', right: 'ro', title: 'Romanian' },
+      ],
+    },
+  },
+};
+
 export const parameters = {
   actions: { yargTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -12,13 +27,13 @@ export const parameters = {
 };
 
 export const decorators = [
-  () => ({
+  (_, { globals }) => ({
     i18n,
     beforeCreate() {
       // eslint-disable-next-line no-underscore-dangle
       this.$root._i18n = this.$i18n;
+      this.$i18n.locale = globals.locale;
     },
     template: '<story/>',
   }),
 ];
-
